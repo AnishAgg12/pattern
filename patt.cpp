740e773f4c3d280b91e28d5e1a16db102039617b
Anish Aggarwal <ishitaaggarwal8@gmail.com>
2019-10-01 00:49:41 +0530

    Update patt.cpp

--- patt.cpp
+++ patt.cpp
@@ -1,27 +1,19 @@
 #include<iostream>
-#include<conio.h>
 using namespace std;
-int main()
-{
-
+int main(){
 int i,j,k,l;
-for(i=6;i>=1;i--)
-{
-for(j=1;j<i;j++)
-{
-cout<<"  ";
-}
-for(k=6;k>=i;k--)
-{
-cout<<"*"<<" ";
-}
-for(l=i+1;l<=6;l++)
-{
-cout<<"+"<<" ";
-}
+for(i=6;i>=1;i--){
+  for(j=1;j<i;j++){
+  cout<<"  ";
+  }
+  for(k=6;k>=i;k--){
+  cout<<"*"<<" ";
+  }
+  for(l=i+1;l<=6;l++){
+  cout<<"+"<<" ";
+  }
 cout<<endl;
 }
-getch();
 return 0;
 }
 
